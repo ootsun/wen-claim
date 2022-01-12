e4f22943227d30a4fc49b467ccb9869b706ba084
ootsun <gabriel.delhaye@hotmail.com>
2022-01-12 14:49:02 +0100

    Setup

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,7 @@
+function Header() {
+  return (
+    <h1>Wen claim?!</h1>
+  )
+}
+
+export default Header;
---
+++ src/pages/Home/home.jsx
@@ -0,0 +1,9 @@
+import '../../App.css';
+
+function Home() {
+  return (
+    <div>Salut</div>
+  );
+}
+
+export default Home;

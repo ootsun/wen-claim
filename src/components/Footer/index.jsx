bfe4f125cfb6a73914232c304fd63a5db6506312
ootsun <gabriel.delhaye@hotmail.com>
2022-01-17 13:09:15 +0100

    Fix footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,12 +2,12 @@ function Footer() {
   return (
     <footer className="flex justify-center items-center h-12">
       <p>Feedback, suggestion or bug report?</p>
-      <a href="https://twitter.com/Oo_Tsun" target="_blank" className="ml-2">
+      <a href="https://twitter.com/Oo_Tsun" target="_blank" rel="noreferrer" className="ml-2">
         <img
           src="twitter.svg"
           alt="Twitter"/>
       </a>
-      <a href="https://github.com/ootsun/wen-claim" target="_blank" className="ml-2">
+      <a href="https://github.com/ootsun/wen-claim" target="_blank" rel="noreferrer" className="ml-2">
         <img
           src="github.svg"
           alt="Github"/>

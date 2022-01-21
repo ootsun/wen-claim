c11c96e057f1ee7d1434bae4576773bb669bc78f
ootsun <gabriel.delhaye@hotmail.com>
2022-01-21 15:55:45 +0100

    Add disclaimer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="flex justify-center items-center h-12">
-      <p>Feedback, suggestion or bug report?</p>
+      <p>This tool has limitations. Use it in addition with other information to define your strategies. Feedback, suggestion or bug report?</p>
       <a href="https://twitter.com/Oo_Tsun" target="_blank" rel="noreferrer" className="ml-2">
         <img
           src="twitter.svg"

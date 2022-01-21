327edffaa167beb3b0422f9727a7e7c3585d17cc
ootsun <gabriel.delhaye@hotmail.com>
2022-01-21 22:46:39 +0100

    Responsive design

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,17 +1,14 @@
 function Footer() {
   return (
-    <footer className="flex justify-center items-center h-12">
-      <p>This tool has limitations. Use it in addition with other information to define your strategies. Feedback, suggestion or bug report?</p>
-      <a href="https://twitter.com/Oo_Tsun" target="_blank" rel="noreferrer" className="ml-2">
-        <img
-          src="twitter.svg"
-          alt="Twitter"/>
-      </a>
-      <a href="https://github.com/ootsun/wen-claim" target="_blank" rel="noreferrer" className="ml-2">
-        <img
-          src="github.svg"
-          alt="Github"/>
-      </a>
+    <footer className="flex justify-center items-center h-max lg:h-12">
+      <p className="text-center">This tool has limitations. Use it in addition with other information to define your strategies. Feedback, suggestion or bug report?
+        <a href="https://twitter.com/Oo_Tsun" target="_blank" rel="noreferrer" className="ml-2">
+          <img src="twitter.svg" alt="Twitter" className="inline"/>
+        </a>
+        <a href="https://github.com/ootsun/wen-claim" target="_blank" rel="noreferrer" className="ml-2">
+          <img src="github.svg" alt="Github" className="inline"/>
+        </a>
+      </p>
     </footer>
   );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <header className="h-10 flex items-center justify-center">
+    <header className="h-16 flex items-center justify-center">
       <h1 className="text-center text-4xl font-cursive">
         <span className="m-1">Wen claim</span>
         <span className="text-amber-500">?</span>
